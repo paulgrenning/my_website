3aebebed7c42c645417b4fad2d05c1b75072df95
Paul <paul@BlueDwarfUnix.(none)>
2010-05-27 09:49:47 -0500

    removed database yml from git and updated gems in the environment

--- config/environment.rb
+++ config/environment.rb
@@ -15,8 +15,9 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem "rspec",       :lib => false, :version => ">= 1.2.0"
-  config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
+  config.gem "rspec",        :lib => false,      :version => ">= 1.2.0"
+  config.gem "rspec-rails",  :lib => false,      :version => ">= 1.2.0"
+  config.gem "sqlite3-ruby", :lib => "sqlite3",  :version => ">= 1.2.5"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
