b54e8b4500b3ec19ed39c1fe089785a74ecdde9f
Paul <paul@BlueDwarfUnix.(none)>
2010-06-07 16:56:48 -0500

    added custom navigation bar ahead of schedule. Updated my about page to contain some sort of information

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,36 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+    $('#navhome').hover(
+      function() {
+        $(this).stop().animate({ left: '50', width: '476' });
+      },
+      function() {
+        $(this).stop().animate({ left: '0', width: '398' });
+      }
+   );
+    $('#navabout').hover(
+      function() {
+        $(this).stop().animate({ left: '50', width: '428' });
+      },
+      function() {
+        $(this).stop().animate({ left: '0', width: '348' });
+      }
+   );
+    $('#navprojects').hover(
+      function() {
+        $(this).stop().animate({ left: '50', width: '431' });
+      },
+      function() {
+        $(this).stop().animate({  left: '0', width: '351' });
+      }
+   );
+    $('#navcontact').hover(
+      function() {
+        $(this).stop().animate({ left: '50', width: '500'});
+      },
+      function() {
+        $(this).stop().animate({ left: '0', width: '420'});
+      }
+   );
+});
