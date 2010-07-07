ab745f1b673faad4994e87a8c8e0c92473124bc2
Paul <paul@BlueDwarfUnix.(none)>
2010-07-07 12:12:47 -0500

    enhanced the header and footer navigations

---
+++ public/javascripts/navigation.js
@@ -0,0 +1,24 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+
+  initializeGlass();
+
+  function initializeGlass(){
+    $("#navigation-glass").stop().animate({left: $("#footer-navigation li").position().left-18, width: $("#footer-navigation li").width()+35, height: 32});
+  }
+
+  $(".header-navigation-button").hover(function() {
+    $(this).stop().animate({top: 33});
+  });
+
+  $(".header-navigation-button").mouseout(function() {
+    $(this).stop().animate({top: 0});
+  });
+
+  $("#footer-navigation li").hover(function () {
+    var hoveredLink = $(this);
+    $("#navigation-glass").stop().animate({left: hoveredLink.position().left-18, width: hoveredLink.width()+35, height: 32});
+  });
+});
