6ecaf8b26c0f636f9e5025552bce3e593a67f9a8
Paul <paul@BlueDwarfUnix.(none)>
2010-07-08 14:58:45 -0500

    added a footer image also adjusted the header tab one pixel to the right.

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -4,6 +4,8 @@
 $(document).ready(function() {
 
   initializeGlass();
+  var hoveredLink = $("#firstChild");
+  var isMouseHovering = false;
 
   function initializeGlass(){
     $("#navigation-glass").stop().animate({left: $("#footer-navigation li").position().left-18, width: $("#footer-navigation li").width()+35, height: 32});
@@ -18,8 +20,26 @@ $(document).ready(function() {
   });
 
   $("#footer-navigation a").hover(function () {
-    var hoveredLink = $(this);
-    $("#navigation-glass").stop().animate({left: hoveredLink.position().left-18, width: hoveredLink.width()+35, height: 32});
+    isMouseHovering = true;
+    hoveredLink = $(this);
+    $("#navigation-glass").stop().animate({left: hoveredLink.position().left-18, width: hoveredLink.width()+35, height: 32}, function() {
+      if(isMouseHovering == true) {
+        hoveredLink.css({"text-decoration": "underline"}); 
+      }
+    });
     $("a.glass-link").attr("href", hoveredLink.attr("href"));
+  });
+
+  $("#navigation-glass").hover(function () {
+    hoveredLink.css({"text-decoration": "underline"}); 
+  });
+
+  $("#navigation-glass").mouseout(function () {
+    hoveredLink.css({"text-decoration": "none"}); 
+  });
+
+  $("#footer-navigation a").mouseout(function() {
+    isMouseHovering = false;
+    $(this).css({"text-decoration": "none"});
   });
 });
