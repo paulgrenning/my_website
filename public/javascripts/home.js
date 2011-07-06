052b4ec44a1349e8238820e97aae2b78e93c14cf
paulgrenning <paul.grenning@gmail.com>
2011-07-06 15:27:50 -0500

    added the mouse over description for the main page

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -4,17 +4,20 @@
 var ProjectsCarousel = {};
 
 $(document).ready(function() {
-  $("#carousel-images").animate({left: '-=870'}, 1);
   ProjectsCarousel.originalThumb;
   ProjectsCarousel.isAnimating = false;
   ProjectsCarousel.isScrollingSet = true;
   ProjectsCarousel.leftClicked = true;
+  firstSlide(true);
   ProjectsCarousel.scrollInterval = setInterval(slideLeft, 4000);
   function slideRight() {
       if(ProjectsCarousel.isAnimating == false) {
         ProjectsCarousel.isAnimating = true;
         var carousel = $("#carousel-images");
         carousel.animate({left: '-=870', queue: true});
+        var title = carousel.children("a:nth-child(3)").children("img").first().attr("title")
+        var description = carousel.children("a:nth-child(3)").children("img").first().attr("alt")
+        $("#imageDescriptionHolder").html("<h3>"+title+"</h3><p>"+description+"</p>");
         carousel.children("a").first().animate({queue: true}, function() { 
           $(this).remove();
           $(this).appendTo(carousel);
@@ -30,6 +33,9 @@ $(document).ready(function() {
         ProjectsCarousel.isAnimating = true;
         var carousel = $("#carousel-images");
         carousel.animate({left: '+=870', queue: true}, 300);
+        var title = carousel.children("a").first().children("img").first().attr("title")
+        var description = carousel.children("a").first().children("img").first().attr("alt")
+        $("#imageDescriptionHolder").html("<h3>"+title+"</h3><p>"+description+"</p>");
         carousel.children("a").last().animate({queue: true}, function() { 
           $(this).remove();
           $(this).prependTo(carousel);
@@ -40,12 +46,34 @@ $(document).ready(function() {
         });
       }
   }
-  $(".carousel-image").mouseover(function () {
-        $("#imageDescriptionHolder").stop(true, true).animate({"bottom": "0px"});
+  function firstSlide(firstTime) {
+      if(ProjectsCarousel.isAnimating == false) {
+        ProjectsCarousel.isAnimating = true;
+        var carousel = $("#carousel-images");
+        carousel.animate({left: '+=870', queue: true}, 10);
+        var title = carousel.children("a").first().children("img").first().attr("title")
+        var description = carousel.children("a").first().children("img").first().attr("alt")
+        $("#imageDescriptionHolder").html("<h3>"+title+"</h3><p>"+description+"</p>");
+        carousel.children("a").last().animate({queue: true}, function() { 
+          $(this).remove();
+          $(this).prependTo(carousel);
+          $(carousel).css({left: "-870"}) 
+          $(this).animate({queue: true}, 10, function() {
+            ProjectsCarousel.isAnimating = false;
+            if(firstTime == true){
+              firstSlide(false);
+            }
+          });
+        });
+      }
+  }
+  $("#carousel-images").mouseover( function (){
+        $("#imageDescriptionHolder").stop(true, true).animate({"bottom": "0"});
   });
-  $(".carousel-image").mouseout(function () {
-        $("#imageDescriptionHolder").stop(true, true).animate({"bottom": "-70px"});
+  $("#carousel-images").mouseout( function (){
+        $("#imageDescriptionHolder").stop(true, true).animate({"bottom": "-80"});
   });
+
   $("#portfolio").live('mouseover',
     function() {
         ProjectsCarousel.isScrollingSet = false;
