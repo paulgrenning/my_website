9b30b3ba0987ff591573c75cda318e3cde453ecf
paulgrenning <paul.grenning@gmail.com>
2011-07-08 13:34:38 -0500

    changed the header navigation and removed the footer navigation

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -2,44 +2,11 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(document).ready(function() {
-
-  initializeGlass();
-  var hoveredLink = $("#firstChild");
-  var isMouseHovering = false;
-
-  function initializeGlass(){
-    $("#navigation-glass").stop().animate({left: $("#footer-navigation li").position().left-18, width: $("#footer-navigation li").width()+35, height: 32});
-  }
-
   $(".header-navigation-button").hover(function() {
-    $(this).stop().animate({top: 66});
+    $(this).stop(true, false).animate({top: 6, "border-color": "#999"}, 200);
   });
 
   $(".header-navigation-button").mouseout(function() {
-    $(this).stop().animate({top: 56});
-  });
-
-  $("#footer-navigation a").hover(function () {
-    isMouseHovering = true;
-    hoveredLink = $(this);
-    $("#navigation-glass").stop().animate({left: hoveredLink.position().left-18, width: hoveredLink.width()+35, height: 32}, function() {
-      if(isMouseHovering == true) {
-        hoveredLink.css({"text-decoration": "underline"}); 
-      }
-    });
-    $("a.glass-link").attr("href", hoveredLink.attr("href"));
-  });
-
-  $("#navigation-glass").hover(function () {
-    hoveredLink.css({"text-decoration": "underline"}); 
-  });
-
-  $("#navigation-glass").mouseout(function () {
-    hoveredLink.css({"text-decoration": "none"}); 
-  });
-
-  $("#footer-navigation a").mouseout(function() {
-    isMouseHovering = false;
-    $(this).css({"text-decoration": "none"});
+    $(this).stop(true, false).animate({top: 0, "border-color": "#282828"}, 400);
   });
 });
