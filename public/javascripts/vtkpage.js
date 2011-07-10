d34aa7d5718c8a9fcff6fcf96fe72c589fd86e56
paulgrenning <paul.grenning@gmail.com>
2011-07-09 19:31:50 -0500

    removed the mouse over expandable areas and made it a button. That way they can stay open and are easier to navigate through

--- public/javascripts/vtkpage.js
+++ public/javascripts/vtkpage.js
@@ -3,11 +3,26 @@
 
 $(document).ready(function() {
 
-  $(".collapsableArea").mouseover(function() {
+  /*$(".collapsableArea").mouseover(function() {
     var wantedHeight = $(this).attr("normalHeight");
     $(this).stop(true, false).animate({height: wantedHeight}, 600);
   });
   $(".collapsableArea").mouseout(function() {
     $(this).stop(true, false).animate({height: 35}, 600);
+  });
+  */
+  $(".expandArrow").click(function() {
+    if($(this).attr("state") == "closed"){
+      $(this).attr("state", "open");
+      $(this).attr("src", "http://renaissancesoftware.net/paulgrenning/contractArrow.png");
+      var parentDiv = $(this.parentNode.parentNode.parentNode);//.parentNode;
+      var newHeight = parentDiv.attr("normalHeight");
+      parentDiv.stop(true, false).animate({height: newHeight}, 600);
+    } else {
+      $(this).attr("state", "closed");
+      $(this).attr("src", "http://renaissancesoftware.net/paulgrenning/expandArrow.png");
+      var parentDiv = $(this.parentNode.parentNode.parentNode);//.parentNode;
+      parentDiv.stop(true, false).animate({height: 35}, 600);
+    }
   });
 });
